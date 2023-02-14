d9e25beaa12824abef8d168b9fef5c6146d0698c
Preston N <49411983+preston176@users.noreply.github.com>
2023-02-14 07:36:29 +0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,16 @@
+//declaring variables in js
+//declaring string. double quotes
+var firstName = "Pres";
+//declaring integer. No double quotes
+var age = 20;
+//declaring bool. can either be true or false
+var isMale = true;
+var balance = 100.33;
+//declaring date use var [variable name] = new Date(yyyy,mm, dd)
+var dob = new Date(2001, 11, 30);
+
+//printing the variables onto the console
+console.log(firstName);
+console.log(age);
+console.log(isMale);
+console.log(dob);
