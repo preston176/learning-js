ed25de9d6605475f5c2bcff9c3f09f0116b15922
Preston N <49411983+preston176@users.noreply.github.com>
2023-02-14 07:40:11 +0300

    Add files via upload
    get to know types of declared variables using typeof

---
+++ variables-2.js
@@ -0,0 +1,16 @@
+//get type of variables
+//declaring string. double quotes
+var firstName = "Pres";
+//declaring integer. No double quotes
+var age = 20;
+//declaring bool. can either be true or false
+var isMale = true;
+var balance = 100.33;
+//declaring date use var [variable name] = new Date(yyyy,mm, dd)
+var dob = new Date(2001, 11, 30);
+
+//printing the types of declared onto the console
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMale);
+console.log(typeof dob);
