fbd85c86099819e9797cda47f0b8c5a47ff6ab13
Preston N <49411983+preston176@users.noreply.github.com>
2023-02-17 07:26:41 +0300

    added boolean file.js

---
+++ boolean.js
@@ -0,0 +1,25 @@
+//boolean
+var isAdult = true;
+console.log(isAdult);
+if (isAdult){
+    console.log("Is adult");
+}
+else
+{
+    console.log("Is not adult")
+}
+
+//or
+/*if (!isAdult){
+    console.log("Is not adult");
+}
+else
+{
+    console.log("Is adult")
+}*/
+
+/*
+! is used to mean NOT
+
+console.log("!true") - displays false
+console.log("!false") - displays true
