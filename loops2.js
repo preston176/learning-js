9c2147706d586529ff9ccd2b15e7fbfa3ee2b31f
preston176 <prestonnyamweya@gmail.com>
2023-03-12 11:54:47 +0300

    added loops2

---
+++ loops2.js
@@ -0,0 +1,9 @@
+//LOOPS
+var names =["Bob", "James", "Pres", "John"];
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+// i is the index
+/*the loop will increment i by 1 */
