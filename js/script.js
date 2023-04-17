2aac9200051ddac172f96a5c87813cf97ee29150
preston176 <prestonnyamweya@gmail.com>
2023-04-17 11:48:48 +0300

    added

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let obj = { abc: 19 };
+obj.innerObj = {};
+console.log(obj.innerObj.abc);
---
+++ tests/test.js
@@ -0,0 +1,11 @@
+/*let contact = {
+    firstName : "Preston",
+    phoneno : 0799078848,
+}
+
+console.log(typeof(contact.phoneno));*/
+
+/*const budget = 100_000;
+let cash = 1771;
+let total = cash * budget;
+console.log("the total is:", total );*/
