2ece2fd506903db95b0edefa616d4dc81611d5c3
Preston N <prestonnyamweya@gmail.com>
2023-05-07 18:56:26 +0300

    Add files via upload

--- js/practice.js
+++ js/practice.js
@@ -24,4 +24,20 @@ let num1 = 212.3293;
 console.log("the number is" + num1);
 
 //now when rounded off 2 decimal places
-console.log("the number is" + num1.toFixed(2));
+console.log("the number rounded to 2 dp " + num1.toFixed(2));
+
+
+//get current date
+
+let rightNow = new Date();
+let dateString = rightNow.toString();
+
+console.log("current date is " + dateString)
+
+
+//concatenation
+
+let name1 = "milner"
+let name2 = "mayieka"
+
+console.log("name1" + "name2");
